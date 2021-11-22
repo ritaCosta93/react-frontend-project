0a0fd97817573703770612d6f011ff85baa2555a
ritaCosta93 <rita.dev4u@gmail.com>
2021-11-22 12:04:06 +0000

    feature -- update place

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -5,6 +5,7 @@ import {useParams} from 'react-router-dom'
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import {VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH} from '../../shared/util/validators'
+import Card from '../../shared/components/UIElements/Card'
 
 import {useForm} from '../../shared/hooks/form-hook'
 
@@ -24,7 +25,7 @@ const DUMMY_PLACES = [
     },
     {
       id: 'p2',
-      title: 'Empire State Building',
+      title: 'Emp. State Building',
       description: 'One of the most famous sky scrapers in the world!',
       imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
@@ -50,6 +51,7 @@ const UpdatePlace = () => {
 
     const identifiedPlace = DUMMY_PLACES.find( p => p.id === placeId );
 
+    
     const [formState,inputHandler,setFormData] = useForm({
       title:{
         value:identifiedPlace.title,
@@ -62,17 +64,21 @@ const UpdatePlace = () => {
   },true)
 
     useEffect(() => {
-      setFormData({
-        title:{
-          value:identifiedPlace.title,
-          isValid:true
-        },
-        description:{
-          value:identifiedPlace.description,
-          isValid:true
-        }
-      },true);
+    if(identifiedPlace){
+        setFormData({
+          title:{
+            value:identifiedPlace.title,
+            isValid:true
+          },
+          description:{
+            value:identifiedPlace.description,
+            isValid:true
+          }
+        },true);
+      }
+        
       setIsLoading(false);
+      
     },[setFormData,identifiedPlace])
 
     
@@ -81,17 +87,21 @@ const UpdatePlace = () => {
 
     if(!identifiedPlace){
         return (
+          <Card>
         <div className="center">
             <h2>Could not find place!</h2>
         </div>
+        </Card>
         );
     }
 
     if(isLoading){
       return (
+        <Card>
         <div className="center">
             <h2>Loading</h2>
         </div>
+        </Card>
       )
     }
 
